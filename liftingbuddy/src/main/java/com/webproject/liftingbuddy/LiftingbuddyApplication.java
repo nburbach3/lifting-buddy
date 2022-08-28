eb6ae725c4ae32c5b45881b6f613ee5e8e710970
Nick Burbach <nburbach@huskers.unl.edu>
2022-08-28 11:00:55 -0500

    added Springboot to project

---
+++ liftingbuddy/src/main/java/com/webproject/liftingbuddy/LiftingbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.liftingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftingbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftingbuddyApplication.class, args);
+	}
+
+}
---
+++ liftingbuddy/src/test/java/com/webproject/liftingbuddy/LiftingbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.liftingbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftingbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
